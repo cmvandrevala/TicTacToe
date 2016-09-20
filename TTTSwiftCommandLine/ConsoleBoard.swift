524b059dca72fc583221bc34ca287eeda57598c4
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2016-09-20 13:32:09 -0500

    Add Leading Newline Character
     * Add a leading newline character to the console board output
     * This will make it easier to read the board when a computer
       player moves

--- TTTSwiftCommandLine/ConsoleBoard.swift
+++ TTTSwiftCommandLine/ConsoleBoard.swift
@@ -17,7 +17,7 @@ public class ConsoleBoard {
     }
     
     private func emptyFormattedBoard() -> String {
-        return " 0 | 1 | 2 \n===========\n 3 | 4 | 5 \n===========\n 6 | 7 | 8 "
+        return "\n 0 | 1 | 2 \n===========\n 3 | 4 | 5 \n===========\n 6 | 7 | 8 "
     }
     
     private func currentMarks() -> [String] {
