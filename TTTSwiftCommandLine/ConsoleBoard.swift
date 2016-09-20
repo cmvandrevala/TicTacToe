651679968659f20fd318e53933c32ec9f370dbe4
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2016-09-20 13:28:04 -0500

    Refactor ConsoleBoard Class

--- TTTSwiftCommandLine/ConsoleBoard.swift
+++ TTTSwiftCommandLine/ConsoleBoard.swift
@@ -9,24 +9,26 @@ public class ConsoleBoard {
     }
     
     public func formattedBoardForConsole() -> String {
-        var formattedString = " 0 | 1 | 2 \n===========\n 3 | 4 | 5 \n===========\n 6 | 7 | 8 "
-        for (indx, mark) in currentMarks().enumerate() {
-            if mark == "X" || mark == "O" {
-                formattedString = formattedString.stringByReplacingOccurrencesOfString("\(indx)", withString: mark)
-            }
+        var boardString = emptyFormattedBoard()
+        for (index, mark) in currentMarks().enumerate() {
+            boardString = boardString.stringByReplacingOccurrencesOfString("\(index)", withString: mark)
         }
-        return formattedString
+        return boardString
+    }
+    
+    private func emptyFormattedBoard() -> String {
+        return " 0 | 1 | 2 \n===========\n 3 | 4 | 5 \n===========\n 6 | 7 | 8 "
     }
     
     private func currentMarks() -> [String] {
         var marksInCells: [String] = []
-        for status in gameBoard.currentBoard() {
+        for (index,status) in gameBoard.currentBoard().enumerate() {
             if status == .PlayerOne {
                 marksInCells.append("X")
             } else if status == .PlayerTwo {
                 marksInCells.append("O")
             } else {
-                marksInCells.append("")
+                marksInCells.append("\(index)")
             }
         }
         return marksInCells
