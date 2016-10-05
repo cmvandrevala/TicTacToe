ef4504dbee6b81b505750f3b136005e3ed42b04e
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2016-10-05 10:53:54 -0500

    Main menu and skeleton
     * Main menu with human vs. human option
     * Navigation Controller that takes you to human vs. human game

--- TicTacToeiOS/HumanVsHumanViewController.swift
+++ TicTacToeiOS/HumanVsHumanViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class ViewController: UIViewController {
+class HumanVsHumanViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -13,4 +13,3 @@ class ViewController: UIViewController {
     }
 
 }
-
